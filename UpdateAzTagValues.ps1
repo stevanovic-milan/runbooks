d38f63dfbd186c5c3b0f17f641dd6cc9e30f37fa
Milan Stevanović <97511214+stevanovic-milan@users.noreply.github.com>
2022-01-16 18:55:10 +0100

    Create UpdateAzTagValues.ps1
    Initial commit.

---
+++ UpdateAzTagValues.ps1
@@ -0,0 +1,49 @@
+# Requires Automation Account System Managed Identity, Az.Accounts and Az.Resources PS modules
+# <#
+# .SYNOPSIS
+#   Changes Wrong Tag Vaulues of each resource within the given resource group.
+# .DESCRIPTION
+#   Updates all the wrongly assigned tag values withing the resource group.
+#   Created to standardize Resource Tag Values, such as "PRD", "Prod", "Production" into one single value.
+#   To replace multiple Tag Values in one go just add them as space separated values to $WrongTagValues parameter.
+# .NOTES
+#   Version:        1.0
+#   Author:         Milan Stevanovic
+#   Creation Date:  16.01.2022.
+#   Purpose/Change: Initial script development
+# #>
+Param
+(
+  [Parameter (Mandatory= $true)]
+  [String] $ResourceGroupName,
+  [Parameter (Mandatory= $true)]
+  [String] $TagName,
+  [Parameter (Mandatory= $true)]
+  [String] $CorrectTagValue,
+  [Parameter (Mandatory= $true)]
+  [String] $WrongTagValues
+
+)
+# Import the modules
+Import-module 'az.accounts'
+Import-module 'az.resources'
+
+# Connect to Azure with the System Managed Identity
+Connect-AzAccount -Identity
+
+$WrongTagValuesArray = $WrongTagValues.Split(" ")
+$NewTag = @{$TagName=$CorrectTagValue}
+
+# Checking each Wrong Tag Value
+foreach ($TagValue in $WrongTagValuesArray) {
+
+    $WrongTag = @{$TagName=$TagValue}
+    $Resources = @()
+    Write-Output "Selected Resource Group: $($ResourceGroupName)"
+    $Resources = Get-AzResource -ResourceGroupName $ResourceGroupName  -Tag $WrongTag
+
+    foreach ($Resource in $Resources) {
+            Write-Output "Processing resource $($Resource.Name) ..."
+            Update-AzTag -ResourceId $Resource.ResourceId -Tag $NewTag -Operation Merge
+    }
+}
